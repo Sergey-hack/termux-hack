5de03d7e9ea495c1481b5354f20ee4568430ae8f
Sergey-hack <78960974+Sergey-hack@users.noreply.github.com>
2021-02-18 13:49:02 +0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,30 @@
+apt-get update
+apt-get upgrade
+apt update
+apt upgrade
+pkg update
+pkg upgrade
+cd #
+git clone https://github.con/SergoProxy/InfinityBomber
+pkg install tor
+pkg install python2
+git clone https://github.com/evait-security/weeman
+chmod +x setup
+./setup
+cd #
+git clone https://github.com/sabri-zaki/EasY-HaCk
+cd EasY-HaCk
+chmod +x install.sh
+./install.sh
+pkg install python
+mkdir bonus
+cd bonus
+git clone https://github.com/Sergey-hack/prime-number-generator-ru
+git clone https://github.com/Sergey-hack/random-password-generator-ru
+cd #
+git clone https://github.com/Manisso/fsociety
+pip install requests
+pip2 install requests
+cd fsociety
+chmod +x install.sh
+python2 fsociety.py
